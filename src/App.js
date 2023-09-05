2136d0acb17c8a3b740e6e7e30ab2cd7bf459b3d
fer <fernando.pereyra764@gmail.com>
2023-09-05 13:35:25 -0300

    cards  sin funcionalida

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import CardList from './componentes/CardList';
 
 
 function App(){  
+
   return (
-    <div className=''>
-      <CardList titulo={"Choferes disponibles"} tieneBoton='true'/>
+    <div className='App'>
+      <h1>Aplicacion de transporte</h1>
+      <div className='contenedor-principal'>
+        <CardList tittle='CHOFERES' />
+        <CardList tittle='CARGAS' />
+        <CardList tittle='EN VIAJE' />
+      </div>
     </div>
   );
 }
--- src/componentes/CardList.jsx
+++ src/componentes/CardList.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
+import '../style-sheets/CardList.css'
 
-const CardList = () => {
+const CardList = (props) => {
+
+  const [choferes, setChoferes] = useState([])
+  const [cargas, setCargas] = useState([])
+  //hacer un estado para las cargas que se van creando
+  
 
   return (
-    <div className=''>
-
-      <h1>Hoolas</h1>
+    <div className='card-container'>
+    <div className='card-tittle'>
+      <h2 className='tittle'>{props.tittle}</h2>
+    </div>
+      <div className='card'>
+        <div className='lista-choferes'>
+          <ul>
+            <li>Manuel casas</li>
+            <li>Josefina Quiroga</li>
+            <li>Andrea Martinoli</li>
+            <li>Manuel Perez</li>
+            <li>Josefina Gomez</li>
+            <li>Andrea Protti</li>
+          </ul>     
+        </div>
+      </div>
+      <button class="boton-agregar"><span> Agregar </span></button>
     </div>
 
   )
